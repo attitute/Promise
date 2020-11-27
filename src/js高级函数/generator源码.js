b147c58d74d8640cb9e03308ac7a460299f63252
hsy <18296576390@163.com>
2020-11-27 08:05:32 +0800

    generator源码解析非ast语法层

--- src/js高级函数/generator源码.js
+++ src/js高级函数/generator源码.js
@@ -1,19 +1,53 @@
 "use strict";
 
-var _marked = /*#__PURE__*/regeneratorRuntime.mark(read); // read就是刚刚写的generator
+// 该还原不考虑编译层
+class Context {
+  constructor() {
+    this.next = 0;
+    this.done = false;
+  }
+  stop() {
+    this.done = true;
+  }
+  abrupt(v1,v2){
+    this.done = true
+    return v2
+  }
+}
+
+let regeneratorRuntime = {
+  mark(genFunc) {
+    return genFunc; // 最外层generator函数
+  },
+  wrap(innerFn, outerFn) {
+    let it = {};
+    let context = new Context();
+    it.next = function (v) {
+      context.send = v
+      let value = innerFn(context); // 函数返回值就是yiled后面的值
+      return {
+        value,
+        done:context.done,
+      };
+    };
+    return it;
+  },
+};
+
+var _marked = /*#__PURE__*/ regeneratorRuntime.mark(read); // read就是刚刚写的generator
 
 function read() {
   var a, b;
   return regeneratorRuntime.wrap(function read$(_context) {
     while (1) {
-      switch (_context.prev = _context.next) {
+      switch ((_context.prev = _context.next)) {
         case 0:
           _context.next = 2;
           return 1;
 
         case 2:
           a = _context.sent;
-          console.log('a', a);
+          console.log("a", a);
           _context.next = 6;
           return 2;
 
@@ -32,7 +66,7 @@ function read() {
 var t = read();
 
 var _t$next = t.next(),
-    value = _t$next.value;
+  value = _t$next.value;
 
 console.log(t.next(value));
 console.log(t.next(value));
